e8389509a3116f338597d51689730763e68f58f5
joshi <joshi.lucky001@gmail.com>
2026-08-20 23:12:56 +0530

    First Commit

---
+++ web/branding/custom-interface_config.js
@@ -0,0 +1,13 @@
+// Think.4U Custom Interface Config Overrides
+if (typeof interfaceConfig !== 'undefined') {
+  interfaceConfig.APP_NAME = 'Think.4U Meet';
+  interfaceConfig.NATIVE_APP_NAME = 'Think.4U Meet';
+  interfaceConfig.DEFAULT_LOGO_URL = 'images/watermark.png';
+  interfaceConfig.DEFAULT_WELCOME_PAGE_LOGO_URL = 'images/watermark.png';
+  interfaceConfig.BRAND_WATERMARK_LINK = 'https://think4u.org';
+  interfaceConfig.JITSI_WATERMARK_LINK = 'https://think4u.org';
+  interfaceConfig.SHOW_JITSI_WATERMARK = false;
+  interfaceConfig.SHOW_BRAND_WATERMARK = true;
+  interfaceConfig.SHOW_WATERMARK_FOR_GUESTS = false;
+  interfaceConfig.SUPPORT_URL = 'https://think4u.org/contact';
+}
